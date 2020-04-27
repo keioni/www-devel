5781d2bf08225dd4ddb1b0464a74910c2324acaa
keioni <otegami@devel.keys.jp>
2020-04-27 19:00:30 +0900

    update for hosting on S3

--- htdocs/js/redundunt.js
+++ htdocs/js/redundunt.js
@@ -1,6 +1,6 @@
 function showRedunduntMessage() {
     let elem = document.getElementById("redundunt-message");
-    elem.innerHTML = "<p class=\"rounded border p-3 mb-2 bg-info text-white text-sm-center\">省エネのため日本時間の午前1時〜7時の間、EC2インスタンスを停止しています。<br>現在、S3上でホストされた静的コンテンツのみ提供しています。</p>";
+    elem.innerHTML = "<p class=\"rounded border p-3 mb-2 bg-info text-white text-sm-center\">現在EC2インスタンスを停止しており、S3上で静的コンテンツのみ提供しています。</p>";
     let elemV6 = document.getElementById("love-ipv6");
     elemV6.style.display = "none";
 }
