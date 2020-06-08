109c37f4605dea2f143214bb76cbd9458cfbf168
keioni <otegami@devel.keys.jp>
2020-06-08 20:15:39 +0900

    change settings

--- htdocs/js/redundunt.js
+++ htdocs/js/redundunt.js
@@ -1,6 +1,6 @@
 function showRedunduntMessage() {
     let elem = document.getElementById("redundunt-message");
-    elem.innerHTML = "<p class=\"rounded border p-3 mb-2 bg-info text-white text-sm-center\">現在EC2インスタンスを停止しており、S3上で静的コンテンツのみ提供しています。</p>";
+    elem.innerHTML = "<p class=\"rounded border p-3 mb-2 bg-info text-white text-sm-center\">経費削減のため現在EC2インスタンスを停止しており、S3上で静的コンテンツのみ提供しています。</p>";
     let elemV6 = document.getElementById("love-ipv6");
     elemV6.style.display = "none";
 }
