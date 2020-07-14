bb3abf6238373abaa47d29df0af7d417bfc94d81
keioni <otegami@devel.keys.jp>
2020-07-14 22:56:41 +0900

    many minor changes.

--- frontend/js/redundant.js
+++ frontend/js/redundant.js
@@ -1,4 +1,4 @@
-function showRedunduntMessage() {
+function showRedundantMessage() {
     let elem = document.getElementById("redundunt-message");
     elem.innerHTML = "<p class=\"rounded border p-3 mb-2 bg-info text-white text-sm-center\">経費削減のため現在EC2インスタンスを停止しており、S3上で静的コンテンツのみ提供しています。</p>";
     let elemV6 = document.getElementById("love-ipv6");
