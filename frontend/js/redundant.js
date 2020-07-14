7d60d00b382f6fbc2271cff42ceec222b5030540
keioni <otegami@devel.keys.jp>
2020-07-14 22:59:50 +0900

    fix spelling

--- frontend/js/redundant.js
+++ frontend/js/redundant.js
@@ -1,5 +1,5 @@
 function showRedundantMessage() {
-    let elem = document.getElementById("redundunt-message");
+    let elem = document.getElementById("redundant-message");
     elem.innerHTML = "<p class=\"rounded border p-3 mb-2 bg-info text-white text-sm-center\">経費削減のため現在EC2インスタンスを停止しており、S3上で静的コンテンツのみ提供しています。</p>";
     let elemV6 = document.getElementById("love-ipv6");
     elemV6.style.display = "none";
